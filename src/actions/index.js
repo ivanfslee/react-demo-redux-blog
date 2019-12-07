e29ab03b970a22f570286be7d2c73d23ac818f6b
ivanfslee <ivanfslee@gmail.com>
2019-12-07 01:41:39 -0800

    Added UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => { //fetchPosts is a function that returns an async arrow function
     const response = await jsonPlaceholder.get('/posts'); //response will be whatever api jsonPlaceholder returns
-    dispatch({ type: 'FETCH_POSTS', payload: response.data})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data}); //dispatch the action obj
 };
 
+export const fetchUser = (id) => async dispatch => { //id is id of user we want to fetch
+    const response = await jsonPlaceholder.get(`/users/${id}`); 
+    dispatch({ type: 'FETCH_USER', payload: response.data }); //dispatch the action obj
+};
