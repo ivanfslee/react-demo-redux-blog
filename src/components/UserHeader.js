06d9d89d2899904bc35ed8d9f0566ca94eda4d88
ivanfslee <ivanfslee@gmail.com>
2019-12-07 12:21:45 -0800

    Created usersReducer and rendered users to appropriate posts

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,12 +7,19 @@ class UserHeader extends React.Component {
         this.props.fetchUser(this.props.userId);
     }
     render() {
-        return (
-            <div>
-                User Header
-            </div>
-        );
+        // 'find' is built-in javascript method.
+        //we are passing in entire list of users and looking at each one to find a match to the id 
+        const user = this.props.users.find(user => user.id === this.props.userId); //find user that matches the user.id we are looking for
+        
+        if (!user) { //on initial render, user will have no value 
+            return null;
+        }
+        // user is an obj from api response -> action.payload -> reducer -> state
+        return <div className="header">{user.name}</div>
     }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = state => {
+    return { users: state.users }; //this.props === { users: state.users }
+}
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
